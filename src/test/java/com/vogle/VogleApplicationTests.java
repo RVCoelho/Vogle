954cb1be12ea3f9c916d601df89c1e9d32fe4bb0
RVCoelho <rvcoelho0@gmail.com>
2026-04-09 13:56:09 -0300

    test: decouple application context from db setup

--- src/test/java/com/vogle/VogleApplicationTests.java
+++ src/test/java/com/vogle/VogleApplicationTests.java
@@ -3,7 +3,14 @@ package com.vogle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.main.lazy-initialization=true",
+		"spring.autoconfigure.exclude="
+				+ "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
+				+ "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,"
+				+ "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration",
+		"deepl.api.key=test-key"
+})
 class VogleApplicationTests {
 
 	@Test
